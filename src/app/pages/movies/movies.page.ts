d0be833777bd76b87adc6d299d2462e1256125df
myprofil <abt1996@gmail.com>
2019-04-05 14:54:25 -0400

    Add files via upload

---
+++ src/app/pages/movies/movies.page.ts
@@ -0,0 +1,30 @@
+import { MovieService } from './../../service/movie.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { SearchType } from 'src/app/service/movie.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.page.html',
+  styleUrls: ['./movies.page.scss'],
+})
+export class MoviesPage implements OnInit {
+
+  results: Observable<any>;
+  searchTerm='';
+  type : SearchType=SearchType.all;
+
+  constructor(private movieService:MovieService) { }
+
+  ngOnInit() {
+  }
+
+  searchChanged(){
+    this.results = this.movieService.searchData(this.searchTerm, this.type);
+
+    // this.results.subscribe(res=> {
+
+    // });
+  }
+
+}
